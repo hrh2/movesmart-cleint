af7f3caf07b8c7f98fdbc0dee205df273f7edfeb
hrh <gakundohope5@gmail.com>
2024-02-09 11:11:13 +0200

    updates

--- src/services/basic/Profile.jsx
+++ src/services/basic/Profile.jsx
@@ -1,7 +1,7 @@
 import React,{useState,useEffect} from 'react'
 import { Typography } from '@mui/material';
 import Axios from 'axios';
-require('dotenv').config()
+// require('dotenv').config()
 
 
 export default function Profile_discription() {
@@ -10,7 +10,7 @@ export default function Profile_discription() {
   const [location,setLocation] = useState('')
   const [country,setCountry] = useState('')
   const [region,setRegion] = useState('')
-  const weatherkey =process.env.weatherkey
+  const weatherkey ="5b732fb231c14d57a82150342240302"
   // eslint-disable-next-line 
   const [error, setError] = useState('')
   useEffect(() => {
